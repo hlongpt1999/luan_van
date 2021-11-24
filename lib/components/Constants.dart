b8954f352aaa6421062c2f5e56c7d04e754f973f
Phan Hai Long <phanhailong12345678@gmail.com>
2021-11-24 14:32:06 +0700

    Upgrade version to 1.0.5 (24/11/2021)

--- lib/components/Constants.dart
+++ lib/components/Constants.dart
@@ -7,8 +7,8 @@ import 'package:luan_van/screens/login/LoginScreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Const{
-  static final String VERSION = "Beta version 1.0.4";
-  static final String VERSION_DATE = "22/11/2021";
+  static final String VERSION = "Beta version 1.0.5";
+  static final String VERSION_DATE = "24/11/2021";
   static final String LOGIN_PREF = "LoginPref";
 
   static String KEY_FROM = "";
